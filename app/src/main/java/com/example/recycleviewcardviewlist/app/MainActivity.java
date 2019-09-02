fc10894781da187b10d370fbeac4a808f7fade57
Parth <parth1493@gmail.com>
2019-09-01 21:58:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/recycleviewcardviewlist/app/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.recycleviewcardviewlist.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.example.recycleviewcardviewlist.R;
+import com.example.recycleviewcardviewlist.adapter.RecyclerAdapter;
+import com.example.recycleviewcardviewlist.model.Landscape;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Operations on Items");
+        toolbar.inflateMenu(R.menu.menu_main);
+
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerAdapter adapter = new RecyclerAdapter(this, Landscape.getData());
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager mLinearLayoutManagerVertical = new LinearLayoutManager(this); // (Context context, int spanCount)
+        mLinearLayoutManagerVertical.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(mLinearLayoutManagerVertical);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator()); // Even if we dont use it then also our items shows default animation. #Check Docs
+    }
+}
